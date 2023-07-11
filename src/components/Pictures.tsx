0d62e75df0bf0b861fccfec7990e9475b8e7d2f8
BENJAMIN GUIGANTON <benjamin.guiganton@gmail.com>
2023-07-12 07:30:31 +0900

    fix: :art: add the author for pictures in carousel

--- src/components/Pictures.tsx
+++ src/components/Pictures.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 
-import { motion } from 'framer-motion';
-import { staggerContainer, fadeIn } from '../utils/motion';
 import { useAppSelector } from '../GlobalRedux/hooks';
 import { Picture } from '../@types/pictures';
 
@@ -19,6 +17,7 @@ function Pictures({ pictures }: PicturesComponentProps) {
   if (infiniteLoadingInfos || !pictures) {
     return <SimpleLoader />;
   }
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextSlide = () => {
@@ -33,27 +32,6 @@ function Pictures({ pictures }: PicturesComponentProps) {
 
   return (
     <section className="p-8 flex flex-col items-center justify-center w-full gap-5 orbitron-font">
-      {/* <motion.div
-        variants={staggerContainer(0.1, 0.2)}
-        initial="hidden"
-        whileInView="show"
-        viewport={{ once: false, amount: 0.25 }}
-        className="flex flex-col items-center justify-center w-full gap-5"
-      >
-        <div
-          id="landscapes"
-          className="container px-4 mx-auto w-full text-center"
-        >
-          {pictures.map((picture, index) => (
-            <div key={index}>
-              <img src={picture.url} alt={picture.description} />
-              <p>{picture.author}</p>
-              <p>{picture.description}</p>
-            </div>
-          ))}
-        </div>
-      </motion.div> */}
-
       <div
         className="carousel-container w-full relative"
         style={{ height: '45rem' }}
@@ -66,9 +44,12 @@ function Pictures({ pictures }: PicturesComponentProps) {
               alt={pictures[currentIndex].description}
             />
             <div className="carousel-caption absolute bottom-0 left-0 right-0 p-4 bg-black bg-opacity-50 text-white text-center">
-              {pictures.length &&
-                pictures[currentIndex].description.charAt(0).toUpperCase() +
-                  pictures[currentIndex].description.slice(1)}
+              <p>{pictures[currentIndex].author}</p>
+              <p>
+                {pictures.length &&
+                  pictures[currentIndex].description.charAt(0).toUpperCase() +
+                    pictures[currentIndex].description.slice(1)}
+              </p>
             </div>
           </>
         )}
