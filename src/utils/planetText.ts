a6991fe946a76768f0eb11161378dffab427a9cc
Canoral <kaya68@outlook.fr>
2023-07-15 11:48:13 +0200

    refactor: :wrench: Editing the JSON file for planets

--- src/utils/planetText.ts
+++ src/utils/planetText.ts
@@ -1,13 +1,28 @@
-export const planetText = {
-  Mercury:
-    'Mercury is the closest planet to the Sun and the smallest planet in the Solar System. Due to its proximity to the Sun, it has no atmosphere to retain heat, causing drastic temperature variations between day and night. Its surface is heavily cratered and resembles the Moon.',
-  Venus:
-    'Venus is the second planet from the Sun and is similar in size and composition to Earth. It has a thick, toxic atmosphere mainly composed of carbon dioxide with clouds of sulfuric acid. The surface pressure is extremely high, and the temperature is hot enough to melt lead.',
-  Mars: "Mars, often referred to as the 'Red Planet', is the fourth planet from the Sun. It has a thin atmosphere mainly composed of carbon dioxide. Mars features the tallest mountain and the deepest canyon in the Solar System. It has two moons, Phobos and Deimos, and is a target for future human exploration.",
-  Jupiter:
-    'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant, primarily composed of hydrogen and helium. It has a strong magnetic field and dozens of moons, including the four large Galilean moons: Io, Europa, Ganymede, and Callisto.',
-  Saturn:
-    'Saturn is the sixth planet from the Sun and is known for its stunning ring system made of ice particles, rocks, and dust. It is a gas giant like Jupiter, with a composition primarily of hydrogen and helium. Saturn has numerous moons, the largest being Titan.',
-  Neptune:
-    'Neptune is the eighth and farthest planet from the Sun in the Solar System. Like Uranus, it is classified as an ice giant and has a deep blue color due to the methane in its atmosphere. Neptune has a dynamic atmosphere with large storms and high-speed winds. It has several moons, with Triton being the largest.',
-};
+export const planetText = [
+  {
+    mercury: {
+      name: 'Mercury',
+      text: 'Mercury is the closest planet to the Sun and the smallest planet in the Solar System. Due to its proximity to the Sun, it has no atmosphere to retain heat, causing drastic temperature variations between day and night. Its surface is heavily cratered and resembles the Moon.',
+    },
+    venus: {
+      name: 'Venus',
+      text: 'Venus is the second planet from the Sun and is similar in size and composition to Earth. It has a thick, toxic atmosphere mainly composed of carbon dioxide with clouds of sulfuric acid. The surface pressure is extremely high, and the temperature is hot enough to melt lead.',
+    },
+    mars: {
+      name: 'Mars',
+      text: "Mars, often referred to as the 'Red Planet', is the fourth planet from the Sun. It has a thin atmosphere mainly composed of carbon dioxide. Mars features the tallest mountain and the deepest canyon in the Solar System. It has two moons, Phobos and Deimos, and is a target for future human exploration.",
+    },
+    jupiter: {
+      name: 'Jupiter',
+      text: 'Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant, primarily composed of hydrogen and helium. It has a strong magnetic field and dozens of moons, including the four large Galilean moons: Io, Europa, Ganymede, and Callisto.',
+    },
+    saturn: {
+      name: 'Saturn',
+      text: 'Saturn is the sixth planet from the Sun and is known for its stunning ring system made of ice particles, rocks, and dust. It is a gas giant like Jupiter, with a composition primarily of hydrogen and helium. Saturn has numerous moons, the largest being Titan.',
+    },
+    neptune: {
+      name: 'Neptune',
+      text: 'Neptune is the eighth and farthest planet from the Sun in the Solar System. Like Uranus, it is classified as an ice giant and has a deep blue color due to the methane in its atmosphere. Neptune has a dynamic atmosphere with large storms and high-speed winds. It has several moons, with Triton being the largest.',
+    },
+  },
+];
